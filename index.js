c957d5e979704a4bd432cbea95464072f6518e32
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2023-02-14 09:26:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ const server = http.createServer(app)
 
 const io = new Server (server,{
     cors:{
-        origin:'http://localhost:3000',
+        origin:'https://auction-77g0.onrender.com',
         methods:['GET','POST']
     }
 });
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://auction-77g0.onrender.com',
     credentials:true
 
 }))
