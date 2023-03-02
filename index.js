5995bc6bebf4e87c22ac110b986dd1128e4b36aa
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2023-03-02 21:07:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const server = http.createServer(app)
 
 const io = new Server (server,{
     cors:{
-        origin:'http://localhost:3000',
+        origin:'https://strong-biscochitos-e35744.netlify.app',
         methods:['GET','POST']
     }
 });
