2745569e0e18fb3217cd378ce08c37e7595c45de
GOKULAKRISHNAN . J <gokulakrishnan252@gmail.com>
2023-03-16 13:19:10 +0530

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -5,7 +5,7 @@ dotenv.config()
 export const auth = (request,response,next)=>{
     try{
         const token = request.header("my_token");
-        jwt.verify(token,process.env.SECERET_KEY);
+        jwt.verify(token,process.env.SECRET_KEY);
         next();
     }catch (err) {
         response.status(401).send(err.message)
